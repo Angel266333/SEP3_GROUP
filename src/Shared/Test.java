c3fb2c1f9ec3f34e0e038d41a4b94fc9bcef0bb8
kennethp <kennethpete@gmail.com>
2018-10-09 19:14:55 +0200

    A simple test for the JSON serialisation

---
+++ src/Shared/Test.java
@@ -0,0 +1,8 @@
+package Shared;
+
+public class Test {
+	public static void main(String[] args) {
+		MenuItem mi = new MenuItem(42, "crap", "Really crappy", true);
+		System.out.println(mi.toString());
+	}
+}
