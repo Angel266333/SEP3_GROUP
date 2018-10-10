894813a50ff95fccfae0b50328a896ee5acec6a3
kennethp <kennethpete@gmail.com>
2018-10-10 13:54:46 +0200

    Serialization and de-serialization both work

--- src/Shared/MenuItem.java
+++ src/Shared/MenuItem.java
@@ -1,10 +1,17 @@
 package Shared;
 
+import Utils.JsonBuilder;
+import Utils.JsonParser;
+
+import java.util.Map;
+
 public class MenuItem {
 	public int id;
 	public String name;
 	public String description;
 	public boolean isAvailable;
+
+	public MenuItem() {}
 
 	public MenuItem(int id, String name, String description, boolean isAvailable) {
 		this.id = id;
@@ -24,6 +31,9 @@ public class MenuItem {
 	}
 
 	public static MenuItem fromString(String serial) {
-		return null;
+		Map<String, String> map = JsonParser.parse(serial);
+		int id = Integer.parseInt(map.get("id"));
+		boolean ia = ("true".equals(map.get("isAvailable")));
+		return new MenuItem(id, map.get("name"), map.get("description"), ia);
 	}
 }
