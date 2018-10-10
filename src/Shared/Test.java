32f3039ca7fc3f2d75ba68ea2d1ba6a23c0da758
kennethp <kennethpete@gmail.com>
2018-10-10 13:53:52 +0200

    No longer needed

--- src/Shared/Test.java
+++
@@ -1,8 +0,0 @@
-package Shared;
-
-public class Test {
-	public static void main(String[] args) {
-		MenuItem mi = new MenuItem(42, "crap", "Really crappy", true);
-		System.out.println(mi.toString());
-	}
-}
