1ef59c2e4780597ace5c64e19ea5370835bf3d1a
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-10-19 16:34:10 +0200

    some changes to specify database

--- src/Database/ConcreteDatabase.java
+++ src/Database/ConcreteDatabase.java
@@ -19,7 +19,7 @@ public class ConcreteDatabase implements IDatabase
       Class.forName("org.postgresql.Driver");
       
       
-      connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "dana");
+      connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=Kartofil", "postgres", "dana");
    }
    
    
