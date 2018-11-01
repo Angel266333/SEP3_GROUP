0925a4dc5f54528449216d084f9acc267ff1e08c
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-11-01 12:57:31 +0100

    junit test case V1

---
+++ src/test/MenuItemTest.java
@@ -0,0 +1,29 @@
+package test;
+import Shared.MenuItem;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MenuItemTest
+{
+   private MenuItem item;
+   
+   @Before 
+   public void setUp()
+   {
+      item = new MenuItem();
+      
+      item.id = 12;
+      item.description = "food";
+      item.name = "tomato";
+      item.isAvailable = true;
+   }
+   
+   @Test
+   public void test()
+   {
+      MenuItem item2 = MenuItem.fromString(item.toString());
+      
+      assertEquals(item.toString(), item2.toString());
+   }
+ 
+}
