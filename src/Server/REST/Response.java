bf8c0feb4c78a51e1942df14350e354b997f5827
kennethp <kennethpete@gmail.com>
2018-11-06 17:30:28 +0100

    Moved standard responses to separate class and made them static

---
+++ src/Server/REST/Response.java
@@ -0,0 +1,34 @@
+package Server.REST;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Response {
+	public static void OK(HttpExchange httpExchange, byte[] respond) throws IOException {
+		httpExchange.sendResponseHeaders(200, respond.length);
+		OutputStream os = httpExchange.getResponseBody();
+		os.write(respond);
+		os.flush();
+		os.close();
+	}
+
+	public static void badRequest(HttpExchange httpExchange) throws IOException {
+		byte[] respond = "400 Bad Request".getBytes();
+		httpExchange.sendResponseHeaders(400, respond.length);
+		OutputStream os = httpExchange.getResponseBody();
+		os.write(respond);
+		os.flush();
+		os.close();
+	}
+
+	public static void notFound(HttpExchange httpExchange) throws IOException {
+		byte[] respond = "404 Not Found".getBytes();
+		httpExchange.sendResponseHeaders(404, respond.length);
+		OutputStream os = httpExchange.getResponseBody();
+		os.write(respond);
+		os.flush();
+		os.close();
+	}
+}
