feda40ddbf5ab25ed50c05c63dd5d96a118d6ac9
kennethp <kennethpete@gmail.com>
2018-11-15 02:12:09 +0100

    Added basic GET/PUT switch to handle()

--- src/Server/REST/OrderHandler.java
+++ src/Server/REST/OrderHandler.java
@@ -8,10 +8,28 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 
 import static Server.REST.Response.OK;
+import static Server.REST.Response.badRequest;
 
 public class OrderHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
-		//WIP
-}
+		switch(httpExchange.getRequestMethod()) {
+			case "GET":
+				GET(httpExchange);
+				break;
+			case "PUT":
+				PUT(httpExchange);
+				break;
+			default:
+				badRequest(httpExchange);
+		}
+	}
+
+	public void GET(HttpExchange httpExchange) {
+
+	}
+
+	public void PUT(HttpExchange httpExchange) {
+
+	}
 }
