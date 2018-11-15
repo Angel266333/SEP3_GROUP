77552a0904e1d136aa762794ba162042bafb4366
kennethp <kennethpete@gmail.com>
2018-11-15 02:13:21 +0100

    Added Status constants to Order class

--- src/Shared/Order.java
+++ src/Shared/Order.java
@@ -1,9 +1,13 @@
 package Shared;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 import javax.json.Json;
+import javax.print.DocFlavor;
 
 import Utils.JsonMapper;
 
@@ -14,6 +18,27 @@ public class Order implements Serializable {
 	public String comment;
 	public String receipt;
 	public int idTable;
+
+	public static class STATUS {
+		public static final String PENDING = "PENDING";
+		public static final String DELIVERED = "DELIVERED";
+		public static final String PAID = "PAID";
+
+		public static String forName(String name) {
+			switch(name) {
+				case STATUS.DELIVERED:
+					return name;
+				case STATUS.PAID:
+					return name;
+				case STATUS.PENDING:
+					return name;
+				default:
+					return null;
+			}
+		}
+
+	}
+
 
 	public Order() {
 
