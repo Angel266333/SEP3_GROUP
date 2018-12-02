eb485e516092a234a11e78224728d9748925b593
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-12-02 15:09:10 +0100

    client-server connection

--- src/Server/SocketListener.java
+++ src/Server/SocketListener.java
@@ -34,8 +34,12 @@ public class SocketListener implements Runnable {
 			socket = serverSocket.accept();
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
 			writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			writer.write("test");
+			System.out.println("What ever by: KENNETH!");
+			writer.newLine();
 			while(!socket.isClosed()) {
 				String s = reader.readLine();
+				System.out.println(s);
 				command(s);
 			}
 		} catch(IOException e) {
@@ -45,21 +49,22 @@ public class SocketListener implements Runnable {
 
 	public void command(String command) {
 		String[] commands = command.split(" ");
+		System.out.println(command);
 		switch (commands[0]) {
-		// The Web Server sends a string, for example “GETORDER 4” to the Server 
+		// The Web Server sends a string, for example â€œGETORDER 4â€� to the Server 
 		// that in turn is followed by a response from the Server with the specific order data 
 		// that was requested. If the ID is not specified, all orders are returned in the form of an array.
 		case "GETORDER":
 			int id = Integer.parseInt(commands[1]);
 			Order order = server.getOrder(id);
-		// The Web Server sends “GETMENUITEMS” and in turn, the Server sends an array of menu items.
+		// The Web Server sends â€œGETMENUITEMSâ€� and in turn, the Server sends an array of menu items.
 		case "GETMENUITEMS":
 			MenuItem[] items = server.getMenuItems(null);
 			for (MenuItem i : items) {
 				response(i.toString());
 			}
 			response("\n");
-		// The Web Server sends a command “SUBMITORDER”  and in turn, the Server returns an OK response.
+		// The Web Server sends a command â€œSUBMITORDERâ€�  and in turn, the Server returns an OK response.
 		case "SUBMITORDER":
 			//TODO
 		}
@@ -68,6 +73,7 @@ public class SocketListener implements Runnable {
 	public void response(String response) {
 		try {
 			writer.write(response);
+			System.out.println(response);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -80,4 +86,11 @@ public class SocketListener implements Runnable {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void main(String[] args)
+   {
+      Thread t1 = new Thread(new SocketListener(new Server()));
+      t1.start();
+   }
+   
 }
