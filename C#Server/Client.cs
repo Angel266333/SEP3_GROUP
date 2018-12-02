f0f6366e161e9eaf41eecaa8510ac58dd92bfd0e
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-12-02 15:10:13 +0100

    C#Server Socket Communication

---
+++ C#Server/Client.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private TcpClient client;
+        private NetworkStream ns;
+        private byte[] data = new byte[1024];
+        private bool connected = false;
+
+        public Client()
+        {
+        }
+
+        public void Start()
+        {
+            Thread t = new Thread(new ThreadStart(Run));
+            t.Start();
+        }
+
+        private void Run()
+        {
+            while( connected == false )
+            {
+                Console.WriteLine( "Trying to connect" );
+                try
+                {
+                    client = new TcpClient("localhost", 8002);
+                    ns = client.GetStream();
+                    connected = true;
+                }
+                catch ( SocketException )
+                {
+                }
+                Thread.Sleep( 2000 );
+            }
+
+            Console.WriteLine("Connected to server");
+            Console.WriteLine("Type a message to the server or exit to stop");
+
+            while (true)
+            {
+                try
+                {
+                    data = new byte[1024];
+                    int recv = ns.Read(data, 0, data.Length);
+                    string Data = Encoding.ASCII.GetString(data, 0, recv);
+                    Console.WriteLine( "Incoming message: {0} ", Data );
+                }
+                catch ( IOException )
+                {
+                    break;
+                }
+            }
+            ns.Close();
+            client.Close();
+        }
+
+        public void SendMsg(string msg)
+        {
+            if (connected)
+            {
+                byte[] response = new byte[1024];
+                response = Encoding.ASCII.GetBytes(msg);
+                ns.Write(response, 0, response.Length);
+                byte [] re = new byte [1];
+                re[0] = ((byte)'\n');
+                ns.Write(re);
+            }
+        }
+    }
+}
---
+++ C#Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bRunning = true;
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine("CTRL+C detected!\n");
+                bRunning = false;
+            };
+
+            Client client = new Client();
+            client.Start();
+
+            while( true )
+            {
+                try
+                {
+                    while (bRunning)
+                    {
+                        string input = Console.ReadLine();
+                        if (input == "exit")
+                            break;
+
+                        client.SendMsg(input);
+                    }
+                    Console.WriteLine("Disconnecting from server...");
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Unable to connect to server");
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+}
