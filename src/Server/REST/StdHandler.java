75e47a2d0cdea3fb7c282276a7490a60d5ede620
kennethp <kennethpete@gmail.com>
2018-12-04 13:26:26 +0100

    Now requires valid token in request header

--- src/Server/REST/StdHandler.java
+++ src/Server/REST/StdHandler.java
@@ -1,15 +1,22 @@
 package Server.REST;
 
+import Utils.Token;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 
 import static Server.REST.Response.OK;
+import static Server.REST.Response.badRequest;
+import static Server.REST.Response.unauthorized;
 
 public class StdHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
+		if(!Token.validate(httpExchange)) {
+			unauthorized(httpExchange);
+			return;
+		}
 		OK(httpExchange, "Hello World".getBytes());
 	}
 }
