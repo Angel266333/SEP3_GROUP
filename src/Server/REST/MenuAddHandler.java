acdd4694d4ba04b5880181e52191b3e025efccb3
kennethp <kennethpete@gmail.com>
2018-12-04 13:24:16 +0100

    Typo fixed

--- src/Server/REST/MenuAddHandler.java
+++ src/Server/REST/MenuAddHandler.java
@@ -16,34 +16,34 @@ public class MenuAddHandler implements HttpHandler{
 
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
-	
+
 		String body = BodyReader.readString(httpExchange.getRequestBody()); //getting json data
 		ObjectMapper mapper = new ObjectMapper(); //used for json conversion
 		try {
-		MenuItem menuItem = mapper.readValue(body, MenuItem.class);
-		int i = RestListener.server.addMenuItem(menuItem);//converting json data into an object
-		if(i==0) {
-			OK(httpExchange, "Success".getBytes());
-		}
-		else if(i==ERROR.DATABASE_ERROR){
-			badRequest(httpExchange);
-		}
-		else if(i==ERROR.REMOTE_EXCEPTION){
-			internalError(httpExchange);
-		}
-		else {
-			badRequest(httpExchange);
-			
-		}
+			MenuItem menuItem = mapper.readValue(body, MenuItem.class);
+			int i = RestListener.server.addMenuItem(menuItem);//converting json data into an object
+			if(i==0) {
+				OK(httpExchange, "Success".getBytes());
+			}
+			else if(i==ERROR.DATABASE_ERROR){
+				badRequest(httpExchange);
+			}
+			else if(i==ERROR.REMOTE_EXCEPTION){
+				internalError(httpExchange);
+			}
+			else {
+				badRequest(httpExchange);
+
+			}
 		}catch(Exception e)
 		{
 			badRequest(httpExchange);
 		}
-	
-		
-		
+
+
+
 	}
-		
-	
+
+
 
 }
