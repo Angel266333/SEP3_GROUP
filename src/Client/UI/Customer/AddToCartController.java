7d3559d26a76f367c287f85adf802289188d9dfe
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-12-07 14:11:04 +0100

    here is it

--- src/Client/UI/Customer/AddToCartController.java
+++ src/Client/UI/Customer/AddToCartController.java
@@ -3,12 +3,14 @@ package Client.UI.Customer;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class AddToCartController implements Initializable
 {  
-   
+   private Button payButton;
    public Stage stage;
    
    public AddToCartController(Stage stage)
@@ -22,5 +24,9 @@ public class AddToCartController implements Initializable
       
       
    }
-
+   @FXML
+   public void payButton()
+   {
+      
+   }
 }
