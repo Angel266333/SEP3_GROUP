9b4c4b486bb2ef0dde96558be9e2f608f91ea320
jbabiccc <31958016+jbabiccc@users.noreply.github.com>
2018-12-07 11:37:08 +0100

    addToCart fxml

---
+++ src/Client/UI/Customer/AddToCartController.java
@@ -0,0 +1,26 @@
+package Client.UI.Customer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+public class AddToCartController implements Initializable
+{  
+   
+   public Stage stage;
+   
+   public AddToCartController(Stage stage)
+   {
+      this.stage = stage;
+   }
+   
+   @Override
+   public void initialize(URL location, ResourceBundle resources)
+   {
+      
+      
+   }
+
+}
--- src/Client/UI/Customer/MainViewController.java
+++ src/Client/UI/Customer/MainViewController.java
@@ -1,57 +1,78 @@
 package Client.UI.Customer;
 
-import Client.ClientEngine;
-import Shared.MenuItem;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainViewController implements Initializable {
-	@FXML
-	private VBox vBoxId;
+import Client.ClientEngine;
+import Shared.MenuItem;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-	@FXML
-	private ScrollPane scp2;
+public class MainViewController implements Initializable
+{
+   @FXML
+   private VBox vBoxId;
 
-	@FXML
-	private Button SearchButton;
+   @FXML
+   private ScrollPane scp2;
 
-	@FXML
-	private GridPane paneID;
+   @FXML
+   private Button SearchButton;
 
-	public static MenuItem[] menuItems;
-	private Stage stage;
+   @FXML
+   private GridPane paneID;
 
-	public MainViewController(Stage stage) {
-		this.stage = stage;
-	}
+   @FXML
+   private Button ViewCartButton;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		try {
-			menuItems = ClientEngine.getInstance().getMenu();
-		}catch(Exception e) {
-			new Alert(Alert.AlertType.ERROR, "Connection problems", ButtonType.OK).showAndWait();
-		}
-	}
+   public static MenuItem[] menuItems;
 
-	public void loadMenuItems() {
-		int r = 0;
-		for(MenuItem m : menuItems) {
-			paneID.addRow(r++, new MenuItemLabel(m));
-			System.out.println(m.price);
-		}
-	}
+   private Stage stage;
+
+   public MainViewController(Stage stage)
+   {
+      this.stage = stage;
+   }
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources)
+   {
+      try
+      {
+         menuItems = ClientEngine.getInstance().getMenu();
+      }
+      catch (Exception e)
+      {
+         new Alert(Alert.AlertType.ERROR, "Connection problems", ButtonType.OK)
+               .showAndWait();
+      }
+   }
+
+   public void loadMenuItems()
+   {
+      int r = 0;
+      for (MenuItem m : menuItems)
+      {
+         paneID.addRow(r++, new MenuItemLabel(m));
+         System.out.println(m.price);
+      }
+   }
+
+   // @FXML
+   // public void ViewCartButton(ActionEvent e) throws IOException
+   // {
+   //
+   // FXMLLoader loader = new
+   // FXMLLoader(getClass().getResource("AddToCart.fxml"));
+   // loader.setController(new AddToCartController(stage));
+   // stage.setScene(new Scene(loader.load()));
+   //
+   // }
 }
