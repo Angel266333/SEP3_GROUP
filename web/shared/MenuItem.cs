249ba139118c97f0e03772f983578a3164691a33
kennethp <kennethpete@gmail.com>
2018-12-14 15:24:30 +0100

    Web server created

---
+++ web/shared/MenuItem.cs
@@ -0,0 +1,12 @@
+namespace web
+{
+    public class MenuItem
+    {
+        public int id;
+        public string name;
+        public string description;
+        public string[] ingredients;
+        public bool isAvailable;
+        public int price;
+    }
+}
